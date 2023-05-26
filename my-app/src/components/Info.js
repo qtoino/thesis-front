49507c7b9eee4d4c71b906f2c4383cc58ecdd8f5
Francisco Santos <57752852+qtoino@users.noreply.github.com>
2023-05-26 13:29:55 +0200

    test gifs out

--- my-app/src/components/Info.js
+++ my-app/src/components/Info.js
@@ -223,9 +223,9 @@ function Info() {
                 <img className="gif" src={righImage} alt="r" />
                 <br></br>
                 <br></br>
-              </div> */}
+              </div> 
 
-            </div>
+            </div>*/}
 
             <div className="topic">
               <h2>-------TRY THESE TASKS:--------</h2>
