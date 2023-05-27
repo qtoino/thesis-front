f057ba2b0534db3603f04358d5a91255911a4fe1
Francisco Santos <57752852+qtoino@users.noreply.github.com>
2023-05-27 02:18:35 +0200

    fix info

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   const baseBPMPerOneSecond = 60
   const stepsPerBar = 16
-  const beatsPerBar = 8
+  const beatsPerBar = 4
   const barsPerSequence = 1
   const totalSteps = stepsPerBar * barsPerSequence
   const totalBeats = beatsPerBar * barsPerSequence
